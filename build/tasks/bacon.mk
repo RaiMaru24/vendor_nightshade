616d838f0aca3dae1ee54818c64057b51f057d8f
xawlw <abdulazizawlw@gmail.com>
2021-10-16 11:42:16 +0200

    vendor: Change MD5 to SHA256
    * Now we generate SHA256 instead of MD5
    
    Change-Id: I464fd2540005599681e8a896d95f35721f61145e
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -19,10 +19,10 @@
 
 NIGHTSHADE_TARGET_PACKAGE := $(PRODUCT_OUT)/nightshade-$(NIGHTSHADE_VERSION).zip
 
-MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
+SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(NIGHTSHADE_TARGET_PACKAGE)
-	$(hide) $(MD5) $(NIGHTSHADE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(NIGHTSHADE_TARGET_PACKAGE).md5sum
+	$(hide) $(SHA256) $(NIGHTSHADE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(NIGHTSHADE_TARGET_PACKAGE).sha256sum
 	@echo "Package Complete: $(NIGHTSHADE_TARGET_PACKAGE)" >&2
