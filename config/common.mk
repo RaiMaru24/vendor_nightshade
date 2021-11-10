aae2a7f94848fe95e714fd94fc94f54d05a5f4a1
Dmitrii <bankersenator@gmail.com>
2021-11-10 07:39:35 +0000

    vendor: remove aosp and los audio files

--- config/common.mk
+++ config/common.mk
@@ -65,12 +65,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
-# Include AOSP audio files
-include vendor/nightshade/config/aosp_audio.mk
-
-# Include NightShade audio files
-include vendor/nightshade/config/nightshade_audio.mk
-
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
