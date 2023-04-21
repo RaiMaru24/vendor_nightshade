VoltageOS/vendor_voltage

vendor: add overlays
Signed-off-by: Dmitrii <bankersenator@gmail.com>

config/common.mk
@@ -110,6 +110,9 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/voltage/config/partner_gms.mk
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/voltage/overlay
+DEVICE_PACKAGE_OVERLAYS += vendor/voltage/overlay/common
+
 # Versioning
 include vendor/voltage/config/version.mk
 

RaiMaru24/vendor_nightshade

config/common.mk
@@ -110,6 +110,9 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/nightshade/config/partner_gms.mk
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/nightshade/overlay
+DEVICE_PACKAGE_OVERLAYS += vendor/nightshade/overlay/common
+
 # Versioning
 include vendor/nightshade/config/version.mk
 